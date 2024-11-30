fef7ab67933fc9895855a5af3f20a31b903fb584
fernando <ferwar@hotmail.com>
2024-11-30 19:13:29 -0300

    deploy en sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,22 @@ import 'dotenv/config';
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const ALCHEMY_API_KEY_SEPOLIA = process.env.ALCHEMY_API_KEY_SEPOLIA || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY_SEPOLIA}`,
+      accounts: [process.env.PRIVATE_KEY_SEPOLIA || ""],
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY
+    }
+  }
 };
 
 export default config;
