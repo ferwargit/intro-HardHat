ddada9b4758b219374605456625454f55c62e90f
fernando <ferwar@hotmail.com>
2024-11-30 23:04:19 -0300

    script ajuste

--- scripts/kipu.ts
+++ scripts/kipu.ts
@@ -4,6 +4,7 @@ async function main() {
     const accounts = await hre.ethers.getSigners();
     const provider = hre.ethers.provider;
     const RECEIPT_ADDRESS = "0x21AA0AD5E70B5c09b2477724a936e0334351edec";
+    const LOCK_ADDRESS_SEPOLIA = "0x89e28c5C9B8EE9Be1590b599924ab236dc0238Ce";
 
     const blockNumber = await hre.ethers.provider.getBlockNumber();
     console.log("main - blockNumbre:", blockNumber);
@@ -13,13 +14,22 @@ async function main() {
         const accountBalance = await provider.getBalance(account.address);
         console.log('main - accountBalace:', accountBalance);
 
-        const sendAmount = Number(accountBalance) * 0.02; // 2 % of the balance
-        const tx = await account.sendTransaction({
-            to: RECEIPT_ADDRESS,
-            value: BigInt(Math.round(sendAmount)),
-        });
-        const receipt = await tx.wait();
-        console.log("main - receipt:", receipt);
+        const lockContract = await hre.ethers.getContractAt("Lock", LOCK_ADDRESS_SEPOLIA, account);
+        // console.log('main - lockContract:', lockContract);
+
+        const lockOwner = await lockContract.owner();
+        console.log('main - lockOwner:', lockOwner);
+
+        if (lockOwner == account.address) {
+
+            const sendAmount = Number(accountBalance) * 0.02; // 2 % of the balance
+            const tx = await account.sendTransaction({
+                to: RECEIPT_ADDRESS,
+                value: BigInt(Math.round(sendAmount)),
+            });
+            const receipt = await tx.wait();
+            console.log("main - receipt:", receipt);
+        }
     }
 }
 
